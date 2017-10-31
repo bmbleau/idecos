1ba0bd3fcd6c70b0322fb8d204e621e2e8e9c7cd
Brandon Bleau <bmbleau@gmail.com>
2017-10-31 00:22:18 -0400

    Add Python and JSON to language map

--- src/app/editor/language.map.ts
+++ src/app/editor/language.map.ts
@@ -4,9 +4,12 @@ export const EditorLanguageMap = {
   'js': 'javascript',
   'ts': 'typescript',
   'md': 'markdown',
+  'py': 'python',
 
   // Language to Language
   'text': 'text',
+  'json': 'json',
+  'python': 'python',
   'css': 'css',
   'javascript': 'javascript',
   'typescript': 'typescript',
