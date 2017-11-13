790f711eaa50f6034566c5ab06d59e43a85b91d7
Brandon Bleau <bmbleau@gmail.com>
2017-11-13 07:49:05 -0500

    Fix issue where directories where not being created

--- src/app/editor/directory/directory.component.ts
+++ src/app/editor/directory/directory.component.ts
@@ -37,6 +37,7 @@ export class DirectoryComponent {
         {},
         commonModalShell,
         {
+          type: this.entry.isFile ? 'file' : 'folder',
           component: NewEntryComponent,
         }
       );
@@ -53,7 +54,7 @@ export class DirectoryComponent {
 
 
       this.newFileModalId = this.ModalService.register(newEntryModalShell);
-      this.newFolderModalId = this.newFileModalId;
+      this.newFolderModalId = this.ModalService.register(newEntryModalShell);
       this.removeEntryModalId = this.ModalService.register(removeEntryModalShell);
     }
   }
