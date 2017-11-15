6f12211cbaad356cd8b4ea854ed356c131e108ca
Brandon Bleau <bmbleau@gmail.com>
2017-11-14 19:38:16 -0500

    Add i18n support to the IDE

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { PluginComponent } from '../plugin/plugin.component';
+import { WindowService } from '../window.service';
 
 @Component({
   selector: 'settings',
@@ -8,4 +9,12 @@ import { PluginComponent } from '../plugin/plugin.component';
 })
 export class SettingsComponent implements PluginComponent {
   @Input() metadata: any;
+
+  constructor(
+    private window: WindowService,
+  ) {}
+
+  get i18n() {
+    return this.window.i18n;
+  }
 }
