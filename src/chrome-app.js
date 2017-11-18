20b780df03606ea0083c5122ed47bf5ffb65ccfb
Brandon Bleau <bmbleau@gmail.com>
2017-11-17 23:33:46 -0500

    Force chrome app to be a single instance
    This prevents the IDECOS Chrome app from opening multiple windows
    so that later we can open files within the active window from the
    file manager.

--- src/chrome-app.js
+++ src/chrome-app.js
@@ -1,5 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
+    'id': 'net.bleauweb.idecos',
+    'singleton': true,
     'outerBounds': {
       'width': 1000,
       'height': 600
